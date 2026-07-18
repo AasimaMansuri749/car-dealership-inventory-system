2bdbe72c2966f4c01b4bdf553bc94050156b06f1
AASIMAMANSURI <136147114+AASIMAMANSURI@users.noreply.github.com>
2026-07-18 11:27:45 +0530

    add user and vehicle models

---
+++ backend/app/models/user.py
@@ -0,0 +1,12 @@
+from sqlalchemy import String
+from sqlalchemy.orm import Mapped, mapped_column
+from app.database.database import Base
+
+class User(Base):
+    __tablename__ = "users"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    username: Mapped[str] = mapped_column(String, nullable=False)
+    email: Mapped[str] = mapped_column(String, unique=True, nullable=False)
+    password_hash: Mapped[str] = mapped_column(String, nullable=False)
+    role: Mapped[str] = mapped_column(String, default="CUSTOMER", nullable=False)
